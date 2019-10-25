3df6333e3d2bc934bf3678722317e432bcfbf16f
Junior Dugue <jrdugue@gmail.com>
2019-10-25 15:07:10 -0400

    created add to cart btn, wrapped the omg, title, button and price within text-center thumbnail div, created util file with function to allow the price to be formatted, changed cwm to cdm, created sort method for the filteredProducts functionality, and added a key to the map function

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,20 +1,39 @@
 import React, {Component} from 'react';
 import Products from "./components/Products";
 import './App.scss';
+import Filter from './components/Filter';
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {products: [], filteredProducts: [],}
+    this.state = {products: [], filteredProducts: [],};
+    this.handleChangeSort = this.handleChangeSort.bind(this);
   }
 
-  componentWillMount(){
+  componentDidMount(){
     fetch("http://localhost:8000/products").then(res => res.json())
     .then(data => this.setState({
       products: data,
       filteredProducts: data,
     }))
   }
+
+  handleChangeSort(e){
+    this.setState({sort: e.target.value});
+    this.listProducts();
+  }
+
+  listProducts(){
+    this.setState(state => {
+      if(state.sort !== ''){
+        state.products.sort((a,b) => (state.sort === 'lowest' ? ((a.price > b.price) ? 1 : -1) : ((a.price < b.price) ? 1 : -1)));
+      } else {
+        state.products.sort((a,b) => (a.id > b.id) ? 1 : -1);
+      }
+      return {filteredProducts: state.products}
+    })
+  }
+
   render(){
       return (
         <div className="container">
@@ -23,6 +42,8 @@ class App extends Component {
 
           <div className="row">
             <div className="col-md-8">
+              <Filter size={this.state.size} sort={this.state.sort} handleChangeSize={this.handleChangeSize} handleChangeSort={this.handleChangeSort} count={this.state.filteredProducts.length}/>
+              <hr/>
               <Products products={this.state.filteredProducts} handleAddToCart={this.handleAddToCart}/>
             </div>
             <div className="col-md-4">
